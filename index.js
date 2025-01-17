d642fd3e983ea1dc01bbd764c354efdbfccd7d9c
ChongOscar <ohlookoscarshere@gmail.com>
2025-01-17 00:09:06 -0500

    finished

--- index.js
+++ index.js
@@ -2,178 +2,227 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
-import { collection, addDoc, serverTimestamp} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  where,
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 /* === Firebase Setup === */
 const firebaseConfig = {
-    apiKey: "AIzaSyBkpnZ5dDrSVpVzAjRxOR0ftgWm1kg6UuM",
-    authDomain: "hot-and-cold-b02cc.firebaseapp.com",
-    projectId: "hot-and-cold-b02cc",
-    storageBucket: "hot-and-cold-b02cc.firebasestorage.app",
-    messagingSenderId: "147130792942",
-    appId: "1:147130792942:web:d3c7d2cdaa13a67d966cff"
-  };
+  apiKey: "AIzaSyBkpnZ5dDrSVpVzAjRxOR0ftgWm1kg6UuM",
+  authDomain: "hot-and-cold-b02cc.firebaseapp.com",
+  projectId: "hot-and-cold-b02cc",
+  storageBucket: "hot-and-cold-b02cc.firebasestorage.app",
+  messagingSenderId: "147130792942",
+  appId: "1:147130792942:web:d3c7d2cdaa13a67d966cff",
+};
 const app = initializeApp(firebaseConfig);
 console.log(app.options.projectId);
-const auth = getAuth(app)
-console.log(auth)
+const auth = getAuth(app);
+console.log(auth);
 const db = getFirestore(app);
-console.log(db)
+console.log(db);
 /* === UI === */
 
 /* == UI - Elements == */
-const userProfilePictureEl = document.getElementById("user-profile-picture")
+const userProfilePictureEl = document.getElementById("user-profile-picture");
 
-const viewLoggedOut = document.getElementById("logged-out-view")
-const viewLoggedIn = document.getElementById("logged-in-view")
+const viewLoggedOut = document.getElementById("logged-out-view");
+const viewLoggedIn = document.getElementById("logged-in-view");
 
-const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+const signInWithGoogleButtonEl = document.getElementById(
+  "sign-in-with-google-btn"
+);
 
-const emailInputEl = document.getElementById("email-input")
-const passwordInputEl = document.getElementById("password-input")
+const emailInputEl = document.getElementById("email-input");
+const passwordInputEl = document.getElementById("password-input");
 
-const signInButtonEl = document.getElementById("sign-in-btn")
-const createAccountButtonEl = document.getElementById("create-account-btn")
+const signInButtonEl = document.getElementById("sign-in-btn");
+const createAccountButtonEl = document.getElementById("create-account-btn");
 
-const signOutButtonEl = document.getElementById("sign-out-btn")
+const signOutButtonEl = document.getElementById("sign-out-btn");
 
-const userGreetingEl = document.getElementById("user-greeting")
+const userGreetingEl = document.getElementById("user-greeting");
 
-const textareaEl = document.getElementById("post-input")
-const postButtonEl = document.getElementById("post-button")
+const postButtonEl = document.getElementById("post-button");
+
+const searchBarEl = document.getElementById("search-bar");
 /* == UI - Event Listeners == */
 
-signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle);
 
-signInButtonEl.addEventListener("click", authSignInWithEmail)
-createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+signInButtonEl.addEventListener("click", authSignInWithEmail);
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail);
 
-signOutButtonEl.addEventListener("click", authSignOut)
-postButtonEl.addEventListener("click", goToPostPage)
+signOutButtonEl.addEventListener("click", authSignOut);
+postButtonEl.addEventListener("click", goToPostPage);
+
+searchBarEl.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    document.querySelector(".post-section").innerHTML = "";
+    showPosts(searchBarEl.value);
+  }
+});
 
 /* === Main Code === */
 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-      showLoggedInView()
-      showProfilePicture(userProfilePictureEl, user)
-      showUserGreeting(userGreetingEl, user)
-      showPosts()
-    } else {
-      showLoggedOutView()
-    }
-  });
+  if (user) {
+    showLoggedInView();
+    showProfilePicture(userProfilePictureEl, user);
+    showUserGreeting(userGreetingEl, user);
+    showPosts();
+  } else {
+    showLoggedOutView();
+  }
+});
 
-  
 /* === Functions === */
 
 function showProfilePicture(imgElement, user) {
-    if (user !== null) {
-        if (user.photoURL != null) {
-            imgElement.src = user.photoURL;
-        } else {
-            imgElement.src = "assets/images/defaultPic.jpg";
-        }
+  if (user !== null) {
+    if (user.photoURL != null) {
+      imgElement.src = user.photoURL;
+    } else {
+      imgElement.src = "assets/images/defaultPic.jpg";
     }
- }
- 
+  }
+}
 
- function showUserGreeting(element, user) {
-
-    if (user !== null) {
-        if (user.displayName != null) {
-            element.textContent = `Welcome ${user.displayName}`;
-        } else {
-            element.textContent = "Welcome";
-        }
+function showUserGreeting(element, user) {
+  if (user !== null) {
+    if (user.displayName != null) {
+      element.textContent = `Welcome ${user.displayName}`;
+    } else {
+      element.textContent = "Welcome";
     }
- }
+  }
+}
 
 function goToPostPage() {
-  window.location.href = "post.html"
+  window.location.href = "post.html";
 }
- 
+
+function formatDate(date) {
+  return `${date.toLocaleString("default", {
+    month: "long",
+  })} ${date.getDate()}, ${date.getHours()}:${
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+  }`;
+}
+
 /* = Functions - Firebase - Authentication = */
 
 function authSignInWithGoogle() {
-    console.log("Sign in with Google")
+  console.log("Sign in with Google");
 }
 
 function authSignInWithEmail() {
-    console.log("Sign in with email and password")
+  console.log("Sign in with email and password");
 
-   const email = emailInputEl.value;
-   const password = passwordInputEl.value;
-   signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    showLoggedInView()
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage)
-  });
+  const email = emailInputEl.value;
+  const password = passwordInputEl.value;
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      showLoggedInView();
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
 }
 
-
 function authCreateAccountWithEmail() {
-    console.log("Sign up with email and password")
+  console.log("Sign up with email and password");
 
-   const email = emailInputEl.value;
-   const password = passwordInputEl.value;
-   createUserWithEmailAndPassword(auth, email, password)
-   .then((userCredential) => {
-    showLoggedInView()
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode)
-    console.log(errorMessage)
-  });
+  const email = emailInputEl.value;
+  const password = passwordInputEl.value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      showLoggedInView();
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
 }
 
 function authSignOut() {
-    signOut(auth).then(() => {
-        showLoggedOutView();
-      }).catch((error) => {
-        console.log(error.message)
-      });
- }
- 
+  signOut(auth)
+    .then(() => {
+      showLoggedOutView();
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
 
 /* == Functions - UI Functions == */
 
-async function showPosts() {
-  const querySnapshot = await getDocs(collection(db, "Posts"));
+async function showPosts(searchOption) {
+  const q = query(collection(db, "Posts"), orderBy("createdAt", "desc"));
+  const querySnapshot = await getDocs(q);
+
   querySnapshot.forEach((doc) => {
-    
-});
+    console.log(doc.id, " => ", doc.data());
+    const post = doc.data();
+    let postName =
+      post.uid.displayName == null ? "Anonymous" : post.uid.displayName;
+    if (searchOption != null) {
+      if (
+        !post.body.toLowerCase().match(searchOption.toLowerCase()) &&
+        !postName.toLowerCase().match(searchOption.toLowerCase())
+      )
+        return;
+    }
+    let postProfilePicture =
+      post.uid.photoURL == null
+        ? "assets/images/defaultPic.jpg"
+        : post.uid.photoURL;
+    let postTimestamp = post.createdAt.toDate();
+    postTimestamp = formatDate(postTimestamp);
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = `
+        <div class="post-header">
+            <img id="post-profile-picture" src="${postProfilePicture}">
+            <p class="post-username" id="post-name">${postName}</p>
+            <p class="post-timestamp">${postTimestamp}</p>
+        </div>
+        <hr/>
+        <div class="post-body">
+            <p class="body-text">${post.body}</p>
+        </div>
+        `;
+    document.querySelector(".post-section").appendChild(postElement);
+  });
 }
 
 function showLoggedOutView() {
-    hideView(viewLoggedIn)
-    showView(viewLoggedOut)
- }
- 
- 
- function showLoggedInView() {
-    hideView(viewLoggedOut)
-    showView(viewLoggedIn)
- }
- 
- 
- function showView(view) {
-    view.style.display = "flex"
- }
- 
- 
- function hideView(view) {
-    view.style.display = "none"
- }
- 
+  hideView(viewLoggedIn);
+  showView(viewLoggedOut);
+}
+
+function showLoggedInView() {
+  hideView(viewLoggedOut);
+  showView(viewLoggedIn);
+}
+
+function showView(view) {
+  view.style.display = "flex";
+}
+
+function hideView(view) {
+  view.style.display = "none";
+}
 
 //credit: coursera
--- post.js
+++ post.js
@@ -1,162 +1,156 @@
 /* === Imports === */
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { signInWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { signOut } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
-import { collection, addDoc, serverTimestamp} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  where,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 import { getDocs } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 /* === Firebase Setup === */
 const firebaseConfig = {
-    apiKey: "AIzaSyBkpnZ5dDrSVpVzAjRxOR0ftgWm1kg6UuM",
-    authDomain: "hot-and-cold-b02cc.firebaseapp.com",
-    projectId: "hot-and-cold-b02cc",
-    storageBucket: "hot-and-cold-b02cc.firebasestorage.app",
-    messagingSenderId: "147130792942",
-    appId: "1:147130792942:web:d3c7d2cdaa13a67d966cff"
-  };
+  apiKey: "AIzaSyBkpnZ5dDrSVpVzAjRxOR0ftgWm1kg6UuM",
+  authDomain: "hot-and-cold-b02cc.firebaseapp.com",
+  projectId: "hot-and-cold-b02cc",
+  storageBucket: "hot-and-cold-b02cc.firebasestorage.app",
+  messagingSenderId: "147130792942",
+  appId: "1:147130792942:web:d3c7d2cdaa13a67d966cff",
+};
 const app = initializeApp(firebaseConfig);
 console.log(app.options.projectId);
-const auth = getAuth(app)
-console.log(auth)
+const auth = getAuth(app);
+console.log(auth);
 const db = getFirestore(app);
-console.log(db)
+console.log(db);
 /* === UI === */
 
 /* == UI - Elements == */
-const userProfilePictureEl = document.getElementById("user-profile-picture")
+const userProfilePictureEl = document.getElementById("user-profile-picture");
 
-const viewLoggedOut = document.getElementById("logged-out-view")
-const viewLoggedIn = document.getElementById("posts-view")
+const userGreetingEl = document.getElementById("user-greeting");
 
-const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+const textareaEl = document.getElementById("post-input");
+const postButtonEl = document.getElementById("post-btn");
+const homeButtonEl = document.getElementById("home-button")
 
-const emailInputEl = document.getElementById("email-input")
-const passwordInputEl = document.getElementById("password-input")
-
-const signInButtonEl = document.getElementById("sign-in-btn")
-const createAccountButtonEl = document.getElementById("create-account-btn")
-
-const signOutButtonEl = document.getElementById("sign-out-btn")
-
-const userGreetingEl = document.getElementById("user-greeting")
-
-const textareaEl = document.getElementById("post-input")
-const postButtonEl = document.getElementById("post-btn")
-
-const pastPosts = document.getElementById("past-posts")
 /* == UI - Event Listeners == */
-
+postButtonEl.addEventListener("click", postButtonPressed)
+homeButtonEl.addEventListener("click", goToHomePage)
 /* === Main Code === */
 onAuthStateChanged(auth, (user) => {
+  if (user) {
+    showProfilePicture(userProfilePictureEl, user);
+    showUserGreeting(userGreetingEl, user);
+    showPosts(user);
+  } else {
+    window.location.href = "index.html";
+  }
+});
 
-    if (user) {
-      showProfilePicture(userProfilePictureEl, user)
-      showUserGreeting(userGreetingEl, user)
-      showPosts(pastPosts, user)
-
-    } 
-  });
-
-/*
-const { user1 } = require('./index.js');
-console.log(user1)
-showLoggedInView()
-showProfilePicture(userProfilePictureEl, user1)
-showUserGreeting(userGreetingEl, user1)
-showPosts(pastPosts, user1)
-*/
 /* === Functions === */
 
-async function showPosts(pastPosts, user){
-  const querySnapshot = await getDocs(collection(db, "Posts"));
-    querySnapshot.forEach((doc) => {
-    console.log(doc.data().uid);
-    if (doc.data().uid == user.uid){
-      console.log("working")
-      pastPosts.innerHTML+=`<div>` + doc.data().body + `</div>`
-    }
+async function showPosts(user) {
+  const q = query(
+    collection(db, "Posts"),
+    where("uid", "==", user.uid),
+    orderBy("createdAt", "desc")
+  );
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+    const post = doc.data();
+    let postName =
+      post.uid.displayName == null ? "Anonymous" : post.uid.displayName;
+    let postProfilePicture =
+      post.uid.photoURL == null
+        ? "assets/images/defaultPic.jpg"
+        : post.uid.photoURL;
+    let postTimestamp = post.createdAt.toDate();
+    postTimestamp = formatDate(postTimestamp);
+    const postElement = document.createElement("div");
+    postElement.classList.add("post");
+    postElement.innerHTML = `
+        <div class="post-header">
+            <img id="post-profile-picture" src="${postProfilePicture}">
+            <p class="post-username" id="post-name">${postName}</p>
+            <p class="post-timestamp">${postTimestamp}</p>
+        </div>
+        <hr/>
+        <div class="post-body">
+            <p class="body-text">${post.body}</p>
+        </div>
+        `;
+    document.querySelector(".post-section").appendChild(postElement);
   });
 }
 
 function showProfilePicture(imgElement, user) {
-    if (user !== null) {
-        if (user.photoURL != null) {
-            imgElement.src = user.photoURL;
-        } else {
-            imgElement.src = "assets/images/defaultPic.jpg";
-        }
+  if (user !== null) {
+    if (user.photoURL != null) {
+      imgElement.src = user.photoURL;
+    } else {
+      imgElement.src = "assets/images/defaultPic.jpg";
     }
- }
- 
+  }
+}
 
- function showUserGreeting(element, user) {
-
-    if (user !== null) {
-        if (user.displayName != null) {
-            element.textContent = `Hi ${user.displayName}`;
-        } else {
-            element.textContent = "Hi, how are you?";
-        }
+function showUserGreeting(element, user) {
+  if (user !== null) {
+    if (user.displayName != null) {
+      element.textContent = `Hi ${user.displayName}`;
+    } else {
+      element.textContent = "Hi, how are you?";
     }
- }
+  }
+}
 
- function clearInputField(inputField) {
-    inputField.value = ""
- }
- 
+function clearInputField(inputField) {
+  inputField.value = "";
+}
+
+function formatDate(date) {
+  return `${date.toLocaleString("default", {
+    month: "long",
+  })} ${date.getDate()}, ${date.getHours()}:${
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()
+  }`;
+}
+
+function goToHomePage() {
+  window.location.href = "index.html";
+}
 /* = Functions - Firebase - Authentication = */
 
-function authSignInWithGoogle() {
-    console.log("Sign in with Google")
-}
-
-function authSignInWithEmail() {
-    console.log("Sign in with email and password")
-
-   const email = emailInputEl.value;
-   const password = passwordInputEl.value;
-   signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    showLoggedInView()
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage)
-  });
-}
-
-
- 
- async function addPostToDB(postBody, user) {
-
+async function addPostToDB(postBody, user) {
   try {
     const docRef = await addDoc(collection(db, "Posts"), {
       body: postBody,
       uid: user.uid,
-      createdAt: serverTimestamp()
+      createdAt: serverTimestamp(),
     });
+    document.querySelector(".post-section").innerHTML = "";
+    showPosts(user)
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
   }
 }
 
-
 /* == Functions - UI Functions == */
 function postButtonPressed() {
-  const postBody = textareaEl.value
-  const user = auth.currentUser
-  
-  if (postBody) {
-      addPostToDB(postBody, user)
-      clearInputField(textareaEl)
-  }
+  const postBody = textareaEl.value;
+  const user = auth.currentUser;
 
+  if (postBody) {
+    addPostToDB(postBody, user);
+    clearInputField(textareaEl);
+  }
 }
- 
 
 //credit: coursera
